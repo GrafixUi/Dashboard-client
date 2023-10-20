0658355e64b0d3724d7bbfdaabf1069124a58eda
sanjai06 <nsksanjai@gmail.com>
2023-10-21 00:21:32 +0530

    changes in UI

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import classNames from 'classnames'
-import { Link, useLocation,useMatch } from 'react-router-dom'
-import { DASHBOARD_SIDEBAR_LINKS } from '../../lib/constants'
+import { Link, useLocation, useMatch } from 'react-router-dom'
+import { DASHBOARD_SIDEBAR_LINKS } from './sidebar/sidebarlist'
 
 const linkClass =
-    'flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base'
+    'flex items-center gap-2 font-light px-3 py-2 hover:no-underline active:bg-green-500 rounded-sm text-base'
 
 export default function Sidebar() {
     return (
-        <div className="bg-neutral-900 w-[250px] p-3 flex flex-col">
+        <div className="bg-gray-900 w-[250px] p-3 flex flex-col">
             <div className="flex items-center gap-2 px-1 py-3">
-                <span className="text-neutral-200 text-lg">Dashboard</span>
+                <span className="text-white text-lg">Dashboard</span>
             </div>
             <div className="py-8 flex flex-1 flex-col gap-0.5">
                 {DASHBOARD_SIDEBAR_LINKS.map((link) => (
@@ -39,8 +39,8 @@ function SidebarLink({ link }) {
                 to={link.path}
                 className={classNames(
                     pathname === link.path || (matchRoot && pathname === '/')
-                        ? 'bg-neutral-700 text-white'
-                        : 'text-neutral-400',
+                        ? 'bg-green-500 text-white'
+                        : 'text-white',
                     linkClass
                 )}
             >
@@ -48,13 +48,13 @@ function SidebarLink({ link }) {
                 {link.label}
             </Link>
             {link.submenu && (pathname.startsWith(link.path) || matchRoot) && (
-                <div className="ml-6">
+                <div className="ml-6 text-sm">
                     {link.submenu.map((submenuLink) => (
                         <Link
                             key={submenuLink.key}
                             to={submenuLink.path}
                             className={classNames(
-                                pathname === submenuLink.path ? 'bg-neutral-700 text-white' : 'text-neutral-400',
+                                pathname === submenuLink.path ? 'bg-green-500 text-white' : 'text-white',
                                 linkClass
                             )}
                         >
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,9 +5,7 @@ import TransactionChart from '../components/TransactionChart';
 export default function Dashboard() {
 	return (
 		<div className="container flex flex-col gap-4">
-			<div className='bg-gray-100'>
-				<h1 className="p-2 text-[15px]">Hello, User</h1>
-			</div>
+			
 			<DashboardStatsGrid />
 			<div className="flex flex-row gap-4 w-full">
 				<TransactionChart />
