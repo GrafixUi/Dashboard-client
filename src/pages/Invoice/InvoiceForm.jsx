28d2c391a9942ad9dfffe4a9e7619b8b7c46445a
sanjai06 <nsksanjai@gmail.com>
2023-11-03 12:22:12 +0530

    invoice update

--- src/pages/Invoice/InvoiceForm.jsx
+++ src/pages/Invoice/InvoiceForm.jsx
@@ -1,19 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import { uid } from 'uid'
-import InvoiceItem from './InvoiceItem'
 import InvoiceModal from './InvoiceModal'
 import incrementString from './helpers/incrementString'
-// import Custlistbox from './components/Custlistbox'
 import logo from '../../assets/icons8-invoice-94.png'
 import { fetchCustomers } from '../../Redux/actions/Customer/customers'
+import { fetchItems } from '../../Redux/actions/Items/items'
 import { connect } from 'react-redux'
 
 function InvoiceForm({ customers, fetchCustomers }) {
     const [isOpen, setIsOpen] = useState(false)
+    const [selectedItem, setSelectedItem] = useState('')
+    const [itemDetails, setItemDetails] = useState({})
     const [discount, setDiscount] = useState('')
     const [tax, setTax] = useState('')
     const [invoiceNumber, setInvoiceNumber] = useState('0001')
     const [customerName, setCustomerName] = useState('')
+    const [subtotal, setSubtotal] = useState(0)
+    const [discountRate, setDiscountRate] = useState(0)
+    const [taxAmount, setTaxAmount] = useState(0)
+    const [total, setTotal] = useState(0)
+
     const [items, setItems] = useState([
         {
             id: uid(6),
@@ -39,7 +45,6 @@ function InvoiceForm({ customers, fetchCustomers }) {
             }
         ])
     }
-
     const addItemHandler = () => {
         const id = uid(6)
         setItems((prevItem) => [
@@ -52,39 +57,45 @@ function InvoiceForm({ customers, fetchCustomers }) {
             }
         ])
     }
+    const handleItemSelect = (e) => {
+        const selectedName = e.target.value
+        const selectedItem = items.find((itemOption) => itemOption.name === selectedName)
+
+        if (selectedItem) {
+            setSelectedItem(selectedName)
+            setItemDetails(selectedItem)
+        } else {
+            setSelectedItem('')
+            setItemDetails({})
+        }
+    }
 
     const deleteItemHandler = (id) => {
-        setItems((prevItem) => prevItem.filter((item) => item.id !== id))
+        setItems((prevItems) => prevItems.filter((item) => item.id !== id))
     }
 
-    const edtiItemHandler = (event) => {
-        const editedItem = {
-            id: event.target.id,
-            name: event.target.name,
-            value: event.target.value
-        }
-
-        const newItems = items.map((item) => {
-            for (const key in item) {
-                if (key === editedItem.name && item.id === editedItem.id) {
-                    item[key] = editedItem.value
-                }
+    useEffect(() => {
+        console.log("Items:", items);
+        console.log("Discount:", discount);
+        console.log("Tax:", tax);
+    
+        const newSubtotal = items.reduce((prev, curr) => {
+            if (curr.name.trim().length > 0) {
+                return prev + Number(curr.rate) * Math.floor(curr.qty);
             }
-            return item
-        })
-
-        setItems(newItems)
-    }
-
-    const subtotal = items.reduce((prev, curr) => {
-        if (curr.name.trim().length > 0) return prev + Number(curr.rate * Math.floor(curr.qty))
-        else return prev
-    }, 0)
-
-    const taxRate = parseFloat(tax)
-    const discountRate = (discount * subtotal) / 100
-    const taxAmount = (taxRate * subtotal) / 100
-    const total = subtotal - discountRate + taxAmount || 0
+            return prev;
+        }, 0);
+        
+    
+        console.log("New Subtotal:", newSubtotal);
+    
+        setSubtotal(newSubtotal);
+        setDiscountRate((discount * newSubtotal) / 100);
+        setTaxAmount((tax * newSubtotal) / 100);
+        setTotal(newSubtotal - (discount * newSubtotal) / 100 + (tax * newSubtotal) / 100);
+    }, [items, discount, tax]);
+    
+    
 
     useEffect(() => {
         fetchCustomers()
@@ -132,18 +143,6 @@ function InvoiceForm({ customers, fetchCustomers }) {
                                     <input type="date" className="form-control w-px-150 text-sm date-picker" />
                                 </dd>
                             </div>
-                            {/* <div className="flex mt-2">
-                                <dt className="col-sm-6 mb-2 mb-sm-0 text-md-end ps-0 mt-2.5">
-                                    <span className="fw-normal">Due Date:</span>
-                                </dt>
-                                <dd className="col-sm-6 d-flex justify-content-md-end pe-0 ps-0 ps-sm-2">
-                                    <input
-                                        type="date"
-                                        className="form-control w-px-150 text-sm date-picker"
-                                        
-                                    />
-                                </dd>
-                            </div> */}
                         </dl>
                     </div>
                 </div>
@@ -195,28 +194,29 @@ function InvoiceForm({ customers, fetchCustomers }) {
                         </div>
                         <div>
                             <h3 className="font-bold text-sm">Shipping Address</h3>
-                                {customers.map((customer) => {
-                                    if (`${customer.firstName} ${customer.lastName}` === customerName) {
-                                        return (
-                                            <address key={customer.id}>
-                                                {customer.shippingAddress}
-                                                <br />
-                                                {customer.shippingCity}, {customer.shippingState}, {customer.shippingZip}
-                                                <br />
-                                                {customer.shippingCountry}
-                                            </address>
-                                        )
-                                    }
-                                    return null
-                                })}
+                            {customers.map((customer) => {
+                                if (`${customer.firstName} ${customer.lastName}` === customerName) {
+                                    return (
+                                        <address key={customer.id}>
+                                            {customer.shippingAddress}
+                                            <br />
+                                            {customer.shippingCity}, {customer.shippingState}, {customer.shippingZip}
+                                            <br />
+                                            {customer.shippingCountry}
+                                        </address>
+                                    )
+                                }
+                                return null
+                            })}
                         </div>
                     </div>
                 </div>
-                <div className="px-2 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <div className="flex items-center px-1 sm:gap-4 sm:px-0">
-                        <h2 className="text-sm font-medium leading-6 text-gray-900 ">Terms</h2>
+                <div className="px-2 py-2 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0 ">
+                    <div className="flex items-center px-1 gap-2">
+                        <h2 className="text-sm font-medium leading-6 text-gray-900 ">Terms </h2>
                         <select className="text-sm leading-6 bg-slate-100 rounded p-2 w-full text-gray-700 sm:col-span-2 sm:mt-0">
                             <option value="DueonReceipt">Due on Receipt</option>
+                            <option value="DueonReceipt">Recurring</option>
                         </select>
                     </div>
                     <div className="flex items-center space-x-2 p-2">
@@ -251,15 +251,49 @@ function InvoiceForm({ customers, fetchCustomers }) {
                     </thead>
                     <tbody>
                         {items.map((item) => (
-                            <InvoiceItem
-                                key={item.id}
-                                id={item.id}
-                                name={item.name}
-                                qty={item.qty}
-                                rate={item.rate}
-                                onDeleteItem={deleteItemHandler}
-                                onEdtiItem={edtiItemHandler}
-                            />
+                            <tr key={item.id}>
+                                <td className="w-96">
+                                    <div>
+                                        <select
+                                            className="text-sm leading-6 bg-white border rounded p-2 w-full text-gray-700 sm:col-span-2 sm:mt-0"
+                                            value={item.name}
+                                            onChange={(e) => handleItemSelect(e)}
+                                        >
+                                            <option value="">Select an Item</option>
+                                            {items.map((item) => (
+                                                <option key={item.id} value={item.name}>
+                                                    {item.name}
+                                                </option>
+                                            ))}
+                                        </select>
+
+                                        <textarea
+                                            className="form-control p-2 mt-2"
+                                            rows="2"
+                                            placeholder="Item Information"
+                                        ></textarea>
+                                    </div>
+                                </td>
+                                <td className="">
+                                    <input className="p-2 border rounded-lg" type="number" value={1} />
+                                </td>
+                                <td className="relative w-[10px]">
+                                    <input
+                                        className="p-2 border rounded-lg"
+                                        type="number"
+                                        value={itemDetails.rate || 0}
+                                        onChange={(e) => setItemDetails({ ...itemDetails, rate: e.target.value })}
+                                    />
+                                </td>
+                                <td className="pl-5 items-center justify-center">
+                                    <button
+                                        className="rounded-md bg-red-500 p-2 text-white shadow-sm transition-colors duration-200 hover-bg-red-600"
+                                        onClick={() => deleteItemHandler(item.id)}
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
                         ))}
                     </tbody>
                 </table>
@@ -296,12 +330,6 @@ function InvoiceForm({ customers, fetchCustomers }) {
                                 ({discount || '0'}%)${discountRate.toFixed(2)}
                             </span>
                         </div>
-                        {/* <div className="flex w-full justify-between md:w-1/2">
-                            <span className="font-bold">Tax:</span>
-                            <span>
-                                ({tax || '0'}%)${taxRate.toFixed(2)}
-                            </span>
-                        </div> */}
                         <div className="flex w-full justify-between border-t border-gray-900/10  md:w-1/2 text-lg">
                             <span className="font-bold">Total:</span>
                             <span className="font-bold">${total % 1 === 0 ? total : total.toFixed(2)}</span>
@@ -347,41 +375,43 @@ function InvoiceForm({ customers, fetchCustomers }) {
                         onAddNextInvoice={addNextInvoiceHandler}
                     />
                     <div className="space-y-4 py-2">
-                        {/* <div className="space-y-2">
-                            <label className="text-sm font-bold md:text-base" htmlFor="tax">
-                                Tax rate:
-                            </label>
-                            <div className="flex items-center">
-                                <input
-                                    className="w-full rounded-r-none p-2 bg-white shadow-sm"
-                                    type="number"
-                                    name="tax"
-                                    id="tax"
-                                    min="0.01"
-                                    step="0.01"
-                                    placeholder="0.0"
-                                    value={tax}
-                                    onChange={(event) => setTax(event.target.value)}
-                                />
-                                <span className="rounded-r-md bg-gray-200 py-2 px-4 text-gray-500 shadow-sm">%</span>
-                            </div>
-                        </div> */}
                         <div className="space-y-2">
                             <label className="text-sm font-medium " htmlFor="discount">
                                 Discount rate:
                             </label>
                             <div className="flex items-center">
                                 <input
-                                    className="w-full rounded-r-none bg-slate-50 p-2  shadow-sm"
+                                    className="w-full rounded-r-none bg-slate-50 p-2 shadow-sm"
                                     type="number"
                                     name="discount"
-                                    id="discount"
                                     min="0"
                                     step="0.01"
                                     placeholder="0.0"
                                     value={discount}
                                     onChange={(event) => setDiscount(event.target.value)}
                                 />
+
+                                <span className="rounded-r-md bg-gray-200 py-2 px-4 text-gray-500 shadow-sm">%</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="space-y-4 py-2">
+                        <div className="space-y-2">
+                            <label className="text-sm font-medium " htmlFor="discount">
+                                Tax :
+                            </label>
+                            <div className="flex items-center">
+                                <input
+                                    className="w-full rounded-r-none bg-slate-50 p-2 shadow-sm"
+                                    type="number"
+                                    name="tax"
+                                    min="0"
+                                    step="0.01"
+                                    placeholder="0.0"
+                                    value={tax}
+                                    onChange={(event) => setTax(event.target.value)}
+                                />
+
                                 <span className="rounded-r-md bg-gray-200 py-2 px-4 text-gray-500 shadow-sm">%</span>
                             </div>
                         </div>
@@ -399,7 +429,8 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = {
-    fetchCustomers
+    fetchCustomers,
+    fetchItems
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(InvoiceForm)
--- src/pages/Invoice/InvoiceItem.jsx
+++ src/pages/Invoice/InvoiceItem.jsx
@@ -1,83 +1,85 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import InvoiceField from './InvoiceField'
+import { connect } from 'react-redux'
+import { fetchItems } from '../../Redux/actions/Items/items'
 
-const InvoiceItem = ({ id, name, qty, rate, onDeleteItem, onEdtiItem }) => {
-    const deleteItemHandler = () => {
-        onDeleteItem(id)
-    }
+function InvoiceItem({
+  id,
+  
+  onDeleteItem,
+ 
+  items,
 
-    return (
-        <tr>
-            <td className="">
-                
-                <div class=" ">
-                    <select class="form-select item-details mb-3" cellData={{
-                        placeholder: 'Item name',
-                        type: 'text',
-                        name: 'name',
-                        id: id,
-                        value: name
-                    }}>
-                        <option selected disabled>
-                            Select Item
-                        </option>
-                        <option value="App Design">App Design</option>
-                        <option value="App Customization">App Customization</option>
-                        <option value="ABC Template">ABC Template</option>
-                        <option value="App Development">App Development</option>
-                    </select>
-                    <textarea class="form-control p-2" rows="2" placeholder="Item Information"></textarea>
-                </div>
-            </td>
-            <td className="">
-                <InvoiceField
-                    onEditItem={(event) => onEdtiItem(event)}
-                    cellData={{
-                        type: 'number',
-                        min: '1',
-                        name: 'qty',
-                        id: id,
-                        value: qty
-                    }}
-                />
-            </td>
-            <td className="relative w-[10px] ">
-                <InvoiceField
-                    onEditItem={(event) => onEdtiItem(event)}
-                    cellData={{
-                        className: 'text-right ',
-                        type: 'number',
-                        min: '0.01',
-                        step: '0.01',
-                        name: 'rate',
-                        id: id,
-                        value: rate
-                    }}
-                />
-            </td>
-            <td className="pl-5 items-center justify-center">
-                <button
-                    className="rounded-md bg-red-500 p-2 text-white shadow-sm transition-colors duration-200 hover:bg-red-600"
-                    onClick={deleteItemHandler}
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                        />
-                    </svg>
-                </button>
-            </td>
-        </tr>
-    )
+}) {
+  const [selectedItem, setSelectedItem] = useState('');
+  const [itemDetails, setItemDetails] = useState({ rate: 0, quantity: 0 });
+
+  const deleteItemHandler = () => {
+    onDeleteItem(id);
+  };
+
+ 
+
+  return (
+    <tr>
+      <td className="">
+        <div>
+          <select
+            className="text-sm leading-6 bg-white rounded p-2 w-full text-gray-700 sm:col-span-2 sm:mt-0"
+            value={selectedItem}
+            onChange={(e) => setSelectedItem(e.target.value)}
+          >
+            <option value="">Select an Item</option>
+            {items.map((item) => (
+              <option key={item.id} value={item.name}>
+                {item.name}
+              </option>
+            ))}
+          </select>
+          <textarea
+            class="form-control p-2"
+            rows="2"
+            placeholder="Item Information"
+          ></textarea>
+        </div>
+      </td>
+      <td className="">
+        <input
+          className="p-2 border rounded-lg"
+          type="number"
+          value={itemDetails.quantity}
+         
+        />
+      </td>
+      <td className="relative w-[10px]">
+        <input
+          className="p-2 border rounded-lg"
+          type="number"
+          value={itemDetails.rate}
+         
+        />
+      </td>
+      <td className="pl-5 items-center justify-center">
+        <button
+          className="rounded-md bg-red-500 p-2 text-white shadow-sm 
+                    transition-colors duration-200 hover-bg-red-600"
+          onClick={deleteItemHandler}
+        >
+          sad
+        </button>
+      </td>
+    </tr>
+  );
 }
 
-export default InvoiceItem;
+const mapStateToProps = (state) => ({
+  items: state.items.items,
+  loading: state.items.loading,
+  error: state.items.error,
+});
+
+const mapDispatchToProps = {
+  fetchItems,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(InvoiceItem);
