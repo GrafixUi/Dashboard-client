eca5753923fa8ac4d24923d41e129c6c572231a3
sanjai06 <nsksanjai@gmail.com>
2023-11-11 00:40:17 +0530

    auth update

--- src/components/Auth/register.jsx
+++ src/components/Auth/register.jsx
@@ -22,7 +22,7 @@ export default function Login() {
         e.preventDefault()
 
         try {
-            const response = await fetch('http://localhost:5000/api/auth/register', {
+            const response = await fetch('https://dashboard-server-j55a.onrender.com/api/auth/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
