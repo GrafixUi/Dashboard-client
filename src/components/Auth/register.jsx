c20c13d445b9823f5e12f3492dabcfbe9099deae
sanjai06 <nsksanjai@gmail.com>
2023-11-11 00:39:54 +0530

    auth update

--- src/components/Auth/register.jsx
+++ src/components/Auth/register.jsx
@@ -22,7 +22,7 @@ export default function Login() {
         e.preventDefault()
 
         try {
-            const response = await fetch('http://localhost:5000/api/register', {
+            const response = await fetch('http://localhost:5000/api/auth/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
